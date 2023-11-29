de2ae757101cf235c9a0fc332653294475e01808
Vinícius Yoda <vyoda4604@gmail.com>
2023-11-29 11:44:12 -0300

    fim

--- cypress/e2e/paginas.cy.js
+++ cypress/e2e/paginas.cy.js
@@ -1,16 +1,24 @@
-describe('Testando múltiplas páginas', () => { 
-  it('Deve conseguir acessar a página de cartões', {browser: 'edge'}, ()=>{
-    cy.visit('/')
-    cy.getByData('botao-login').click()
-    cy.getByData('email-input').type('neilton@alura.com')
-    cy.getByData('senha-input').type('123456')
-    cy.getByData('botao-enviar').click()
+import { isMobile } from '../support/utils';
 
-    cy.location('pathname').should('eq','/home')
+describe('Testando múltiplas páginas', () => {
+  it('Deve conseguir acessar a página de cartões', () => {
+    cy.login(Cypress.env('email'), Cypress.env('senha'));
 
-    cy.getByData('app-home').find('a').eq(1).click()
-    cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
+    cy.visit('/home');
+    cy.location('pathname').should('eq', '/home');
 
-    cy.location('pathname').should('eq', '/home/cartoes')
-  })
- })
+    if (isMobile()) {
+      cy.getByData('menu-burguer').should('be.visible');
+      cy.getByData('menu-burguer').click();
+      cy.getByData('menu-lateral').find('a').eq(2).click();
+    } else {
+      cy.getByData('app-home').find('a').eq(2).click();
+    }
+
+    cy.getByData('titulo-cartoes')
+      .should('exist')
+      .and('have.text', 'Meus cartões');
+
+    cy.location('pathname').should('eq', '/home/cartoes');
+  });
+});
